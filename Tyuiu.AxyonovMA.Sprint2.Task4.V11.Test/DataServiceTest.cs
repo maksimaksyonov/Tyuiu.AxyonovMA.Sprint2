48e973daf91538b21911034299e0c9122f9940c5
Axyonov Maxim <maksik.aksyonov@mail.ru>
2025-09-27 04:14:06 +0500

    Task4

--- Tyuiu.AxyonovMA.Sprint2.Task4.V11.Test/DataServiceTest.cs
+++ Tyuiu.AxyonovMA.Sprint2.Task4.V11.Test/DataServiceTest.cs
@@ -7,22 +7,27 @@ namespace Tyuiu.AxyonovMA.Sprint2.Task4.V11.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void Calculate_IfBranch()
+        public void Calculate_IfBranch_Example_9_4()
         {
             var ds = new DataService();
-            // x=2, y=3 => condition true => (3 + 8/2)^3 = 7^3 = 343
-            double actual = ds.Calculate(2, 3);
-            Assert.AreEqual(343.000, actual, 0.0001);
+            double actual = ds.Calculate(9, 4);
+            Assert.AreEqual(228.719, actual);
         }
 
         [TestMethod]
-        public void Calculate_ElseBranch()
+        public void Calculate_IfBranch_Small_2_3()
         {
             var ds = new DataService();
-            // x=3, y=-42 => condition false; y - ((x+1)/(y+2))^x
-            // (4 / -40)^3 = (-0.1)^3 = -0.001;  -42 - (-0.001) = -41.999 => -41.999
-            double actual = ds.Calculate(3, -42);
-            Assert.AreEqual(-41.999, actual, 0.0001);
+            double actual = ds.Calculate(2, 3); // (3 + 8/2)^3 = 7^3
+            Assert.AreEqual(343.000, actual);
+        }
+
+        [TestMethod]
+        public void Calculate_ElseBranch_NegativeY()
+        {
+            var ds = new DataService();
+            double actual = ds.Calculate(3, -42); // else-ветка
+            Assert.AreEqual(-41.999, actual);
         }
     }
 }
