c7e2a844901a695ddf4e56d80d58eba71228939f
Axyonov Maxim <maksik.aksyonov@mail.ru>
2025-09-26 21:09:04 +0500

    Task2

---
+++ Tyuiu.AxyonovMA.Sprint2.Task2.V10.Test/DataServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.AxyonovMA.Sprint2.Task2.V10.Lib;
+
+namespace Tyuiu.AxyonovMA.Sprint2.Task2.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea_InShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 5;
+            bool result = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea_NotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            int y = 1;
+            bool result = ds.CheckDotInShadedArea(x, y);
+            bool wait = false;
+            Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea_BorderCase()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            int y = 3;
+            bool result = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.AxyonovMA.Sprint2.Task2.V10/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.AxyonovMA.Sprint2.Task2.V10.Lib;
+
+Console.Title = "Спринт #2 | Выполнил: Аксёнов Максим Алексеевич | ПКТб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Логические операции                                               *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #10                                                             *");
+Console.WriteLine("* Выполнил: Аксёнов Максим Алексеевич | ПКТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает целые значения с клавиатуры и   *");
+Console.WriteLine("* вычисляет находится ли точка с координатами X,Y в заштрихованной области*");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите координату X (целое число от 1 до 15):");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y (целое число от 1 до 15):");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+DataService ds = new DataService();
+bool result = ds.CheckDotInShadedArea(x, y);
+
+if (result)
+{
+    Console.WriteLine($"Точка с координатами ({x},{y}) находится в заштрихованной области");
+}
+else
+{
+    Console.WriteLine($"Точка с координатами ({x},{y}) НЕ находится в заштрихованной области");
+}
+
+Console.ReadKey();
