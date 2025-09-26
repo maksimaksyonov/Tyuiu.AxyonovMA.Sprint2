8e8a4d9031ebbeb6cc48785feabab645e364343e
Axyonov Maxim <maksik.aksyonov@mail.ru>
2025-09-26 20:45:43 +0500

    Task2

--- Tyuiu.AxyonovMA.Sprint2.Task1.V16.Lib/DataService.cs
+++ Tyuiu.AxyonovMA.Sprint2.Task1.V16.Lib/DataService.cs
@@ -8,12 +8,13 @@ namespace Tyuiu.AxyonovMA.Sprint2.Task1.V16.Lib
         {
             bool[] results = new bool[6];
 
-            results[0] = (a < b) | (c > d);
-            results[1] = (a == c + 1) & (b != d);
-            results[2] = (c <= a) || (d >= a);
-            results[3] = (b > c) && (a < d);
-            results[4] = !(d > c);
-            results[5] = (a != b) ^ (c == d);
+            // Операции для получения (True, True, True, False, True, False)
+            results[0] = (a < b) | (c > d);          // True | True → True
+            results[1] = (a == c + 1) & (b != d);    // True & True → True
+            results[2] = (c <= a) || (d >= a);       // True || False → True
+            results[3] = (b > c) && (a < d);         // True && False → False
+            results[4] = !(d > c);                   // !False → True
+            results[5] = (a == b) ^ (c == d);        // False ^ False → False
 
             return results;
         }
